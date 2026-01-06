03c717e57f62c758f439928deefdec86e81402f7
dipghoshraj <dipghoshraj@gmail.com>
2026-01-07 01:24:12 +0530

    agent connect added

--- agni-agent/go.mod
+++ agni-agent/go.mod
@@ -8,6 +8,13 @@ require (
 )
 
 require (
+	github.com/Purple-House/mem-sdk/memsdk v0.0.0-20260105184353-8f197458eb7f
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.9 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.38.0 // indirect
+	golang.org/x/text v0.31.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20251029180050-ab9386a59fda // indirect
+	google.golang.org/grpc v1.78.0 // indirect
+	google.golang.org/protobuf v1.36.11 // indirect
 )
---
+++ agni-agent/pkg/registry/agentconnect.go
@@ -0,0 +1,83 @@
+package registry
+
+import (
+	"context"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/hex"
+	"encoding/pem"
+	"fmt"
+	"log"
+	"os"
+	"time"
+
+	mp "github.com/Purple-House/mem-sdk/memsdk/maps"
+
+	"github.com/Purple-House/mem-sdk/memsdk/pkg"
+)
+
+func AgentFingerprint() (string, error) {
+	permfile := "client.pem"
+	certPEM, err := os.ReadFile(permfile)
+	if err != nil {
+		return "", err
+	}
+
+	block, _ := pem.Decode(certPEM)
+
+	if block == nil || block.Type != "CERTIFICATE" {
+		return "", err
+	}
+
+	cert, err := x509.ParseCertificate(block.Bytes)
+	if err != nil {
+		return "", err
+	}
+
+	sum := sha256.Sum256(cert.Raw)
+	fingerprint := hex.EncodeToString(sum[:])
+	log.Printf("Client CERT fingerprint (SHA256): %s", fingerprint)
+	return fingerprint, nil
+}
+
+func AgentRegistry(agent_domain string, region string) (mp.Agent, string, error) {
+
+	config := pkg.Config{
+		Address:     "localhost:8080",
+		Fingerprint: "86f7b7b55c1591c0aafbb9470baff92f1021791ca8f6ee9e372d0986a886be00",
+		Timeout:     5 * time.Second,
+	}
+
+	client, err := mp.NewSdkOperation(config)
+	if err != nil {
+		return mp.Agent{}, "", err
+	}
+
+	log.Printf("finding the gateways in %s region", region)
+
+	gateways, err := client.GetGatewayInfo(context.Background(), region)
+	if err != nil {
+		return mp.Agent{}, "", err
+	}
+
+	if len(gateways) == 0 {
+		return mp.Agent{}, "", fmt.Errorf("no gateways found")
+	}
+
+	gw := gateways[0]
+	log.Printf("Resolved Gateway: %s (%s)\n", gw.IP, gw.ID)
+
+	fingerprint, err := AgentFingerprint()
+	if err != nil {
+		return mp.Agent{}, "", err
+	}
+	log.Printf("Using Agent Fingerprint: %s\n", fingerprint)
+
+	agent, err := client.ConnectAgent(context.Background(), agent_domain, gw.ID, fingerprint, region)
+	if err != nil {
+		return mp.Agent{}, "", err
+	}
+
+	log.Printf("Connected Agent: %s (%s)\n", agent.ID, agent.Domain)
+	return agent, fingerprint, nil
+}
