4f33908311c2426533db9fc7c37fcb377cc4a8e3
dipghoshraj <dipghoshraj@gmail.com>
2026-01-09 21:56:53 +0530

    load the YAML config

--- agni-router/go.mod
+++ agni-router/go.mod
@@ -1,3 +1,5 @@
 module github.com/Purple-House/agni-tunnels/agni-router
 
 go 1.25.5
+
+require gopkg.in/yaml.v3 v3.0.1
---
+++ agni-router/pkg/config/load_yaml.go
@@ -0,0 +1,45 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/yaml.v3"
+)
+
+type Registry struct {
+	Address      string `yaml:"address"`
+	Fingureprint string `yaml:"fingureprint"`
+}
+
+type Router struct {
+	Name       string   `yaml:"name"`
+	Registry   Registry `yaml:"registry"`
+	Region     string   `yaml:"region"`
+	Certs      string   `yaml:"certs"`
+	RouterIP   string   `yaml:"router_ip"`
+	RouterPort int      `yaml:"port"`
+}
+
+type Config struct {
+	Version string `yaml:"version"`
+	Router  Router `yaml:"Router"`
+}
+
+var YamlConfig *Config
+
+func init() {
+
+	data, err := os.ReadFile("agent-config.yaml")
+	if err != nil {
+		log.Fatalf("error reading YAML file: %v", err)
+	}
+
+	var config Config
+	err = yaml.Unmarshal(data, &config)
+	if err != nil {
+		log.Fatalf("error parsing YAML file: %v", err)
+	}
+
+	YamlConfig = &config
+}
