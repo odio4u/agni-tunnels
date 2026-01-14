28499d280fc69acad9cd1030b4480511bed542bd
dipghoshraj <dipghoshraj@gmail.com>
2026-01-14 14:53:45 +0530

    multipex connections

---
+++ agni-router/pkg/session/handleconnection.go
@@ -0,0 +1,31 @@
+package session
+
+import (
+	"net"
+
+	tunnel "github.com/Purple-House/agni-schema/protobuf"
+	"github.com/Purple-House/agni-tunnels/agni-router/pkg/session/sni"
+)
+
+type TunnleContext struct {
+	connection_id string
+	stream        *tunnel.AgniTunnel_ConnectServer
+	tcp           net.Conn
+	closed        chan struct{}
+}
+
+func HandleStream(conn net.Conn) {
+	// session.AgentRegistry.GetSession()
+
+	serverName, _, err := sni.PeekSNI(conn)
+	if err != nil {
+		return
+	}
+
+	registry, exists := Registry.GetSession(serverName)
+	if !exists {
+		return
+	}
+
+	_ = registry.stream
+}
--- agni-router/pkg/session/session.go
+++ agni-router/pkg/session/session.go
@@ -1,6 +1,8 @@
 package session
 
 import (
+	"log"
+	"sync"
 	"time"
 
 	tunnel "github.com/Purple-House/agni-schema/protobuf"
@@ -8,7 +10,46 @@ import (
 
 type AgentSession struct {
 	AppID    string
-	Conn     *tunnel.AgniTunnel_ConnectServer
+	stream   *tunnel.AgniTunnel_ConnectServer
 	SendChan chan *tunnel.TunnelOpen
 	LastSeen time.Time
+}
+
+type AgentRegistry struct {
+	sync.RWMutex
+	sessions map[string]*AgentSession
+}
+
+var Registry = &AgentRegistry{
+	sessions: make(map[string]*AgentSession),
+}
+
+func (r *AgentRegistry) Register(appID string, session *AgentSession) {
+	r.Lock()
+	defer r.Unlock()
+	r.sessions[appID] = session
+	log.Printf("Agent [%s] registered", appID)
+
+}
+
+func (r *AgentRegistry) Unregister(appID string) {
+	r.Lock()
+	defer r.Unlock()
+	if _, exists := r.sessions[appID]; exists {
+		delete(r.sessions, appID)
+		log.Printf("Agent [%s] unregistered", appID)
+	} else {
+		log.Printf("Attempted to unregister non-existent agent [%s]", appID)
+	}
+}
+
+func (r *AgentRegistry) GetSession(appID string) (*AgentSession, bool) {
+	r.RLock()
+	defer r.RUnlock()
+	session, exists := r.sessions[appID]
+	if !exists {
+		log.Printf("Session for agent [%s] not found", appID)
+		return nil, false
+	}
+	return session, true
 }
