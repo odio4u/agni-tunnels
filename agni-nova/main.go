762d897b28447414035887ee5d7fdefb79e4aabf
dipghoshraj <dipghoshraj@gmail.com>
2026-01-18 03:09:34 +0530

    dedicated the zoo

--- agni-nova/main.go
+++ agni-nova/main.go
@@ -1,1 +1,29 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/odio4u/agni-tunnels/agni-nova/nova"
+)
+
+func main() {
+	log.Println("This is the main entry point for the indraNet reverse proxy.")
+
+	ln, err := net.Listen("tcp", ":443")
+	if err != nil {
+		fmt.Printf("Error starting server: %v\n", err)
+		return
+	}
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Printf("Error accepting connection: %v", err)
+			continue
+		}
+		go nova.HandleStream(conn)
+		log.Printf("Accepted connection from %s", conn.RemoteAddr())
+	}
+}
